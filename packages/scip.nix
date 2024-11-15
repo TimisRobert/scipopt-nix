d13c9827e9821277c5648d70776420399768a3f7
TimisRobert <roberttimis@proton.me>
2024-11-15 15:54:38 +0100

    chore: update

--- packages/scip.nix
+++ packages/scip.nix
@@ -4,7 +4,6 @@
   papilo ? pkgs.callPackage ./papilo.nix {},
   soplex ? pkgs.callPackage ./soplex.nix {},
   zimpl ? pkgs.callPackage ./zimpl.nix {},
-  ipopt-mumps ? pkgs.callPackage ./ipopt-mumps.nix {},
   debugFiles ? [], # add `#define SCIP_DEBUG` to these files (e.g. ["src/scip/cons_linear.c"])
   ...
 }: let
@@ -41,12 +40,8 @@ in
         readline
         soplex
         zlib
+        ipopt
       ])
-      ++ (
-        if ipopt-mumps != null
-        then [ipopt-mumps]
-        else [pkgs.blas pkgs.lapack]
-      )
       ++ (lib.optional (papilo != null) papilo)
       ++ (lib.optional (zimpl != null) zimpl);
 
@@ -73,8 +68,8 @@ in
     cmakeFlags =
       [
         "-D AUTOBUILD=OFF"
+        "-D IPOPT=OFF -D LAPACK=ON"
       ]
-      ++ (lib.optional (ipopt-mumps == null) "-D IPOPT=OFF -D LAPACK=ON")
       ++ (lib.optional (papilo == null) "-D PAPILO=OFF")
       ++ (lib.optional (zimpl == null) "-D ZIMPL=OFF");
 
